bd6eeed30bc0f2523282f5a26fc7f35c16e68590
By_juun <neostgeart@gmail.com>
2024-10-30 21:20:47 +0900

    feat: cart-button 컴포넌트 분리

--- src/app/product-list-page/header/header.component.ts
+++ src/app/product-list-page/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartButtonComponent } from './cart-button/cart-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CartButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
