d949a5555c0ae0ce94287777e4ada302d3ae644d
By_juun <neostgeart@gmail.com>
2024-10-30 13:01:50 +0900

    feat: 헤더마크업 작성

--- src/app/product-list-page/product-list-page.component.ts
+++ src/app/product-list-page/product-list-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-product-list-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ProductListComponent],
   templateUrl: './product-list-page.component.html',
-  styleUrl: './product-list-page.component.css'
+  styleUrl: './product-list-page.component.css',
 })
-export class ProductListPageComponent {
-
-}
+export class ProductListPageComponent {}
